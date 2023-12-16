a122e0c2fa3b21832a9d7e37ebf218ec8840faad
Krishna Praveen <sureshradha084@gmail.com>
2023-12-16 08:01:47 +0000

    Integrated the ReposItem Component in the GitHubPopularRepos Component and With Api Get the List of All Items and built responsive natrue to the items using flex box

--- src/components/GithubPopularRepos/index.js
+++ src/components/GithubPopularRepos/index.js
@@ -1,6 +1,9 @@
 import {Component} from 'react'
 
+import Loader from 'react-loader-spinner'
 import LanguageFilterItem from '../LanguageFilterItem'
+
+import RepositoryItem from '../RepositoryItem'
 
 import './index.css'
 
@@ -14,14 +17,59 @@ const languageFiltersData = [
 
 // Write your code here
 class GithubPopularRepos extends Component {
-  state = {isActiveTabId: languageFiltersData[0].id}
+  state = {
+    isActiveTabId: languageFiltersData[0].id,
+    reposItemList: '',
+    isProgress: true,
+  }
+
+  componentDidMount = () => {
+    this.getRepoList()
+  }
+
+  getRepoList = async () => {
+    const {isActiveTabId} = this.state
+    const apiUrl = `https://apis.ccbp.in/popular-repos?language=${isActiveTabId}`
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    const formattedData = data.popular_repos.map(each => ({
+      name: each.name,
+      id: each.id,
+      avatarUrl: each.avatar_url,
+      forksCount: each.forks_count,
+      issuesCount: each.issues_count,
+      starsCount: each.stars_count,
+    }))
+
+    if (response.ok === true) {
+      this.setState({reposItemList: formattedData, isProgress: false})
+    }
+  }
 
   selectTab = id => {
     this.setState({isActiveTabId: id})
   }
 
+  loader = () => (
+    <div data-testid="loader">
+      <Loader type="ThreeDots" color="#0284c7" height={80} width={80} />
+    </div>
+  )
+
+  listOfItems = () => {
+    const {reposItemList} = this.state
+    return (
+      <ul className="list-item-container">
+        {reposItemList.map(each => (
+          <RepositoryItem reposItemData={each} key={each.id} />
+        ))}
+      </ul>
+    )
+  }
+
   render() {
-    const {isActiveTabId} = this.state
+    const {isActiveTabId, reposItemList, isProgress} = this.state
+    console.log(reposItemList)
     return (
       <div className="bg-container">
         <h1 className="heading">Popular</h1>
@@ -35,6 +83,7 @@ class GithubPopularRepos extends Component {
             />
           ))}
         </ul>
+        {isProgress ? this.loader() : this.listOfItems()}
       </div>
     )
   }
--- src/components/RepositoryItem/index.js
+++ src/components/RepositoryItem/index.js
@@ -1,1 +1,47 @@
 // Write your code here
+import './index.css'
+import {FaStar} from 'react-icons/fa'
+
+const RepositoryItem = props => {
+  const {reposItemData} = props
+  const {
+    id,
+    name,
+    avatarUrl,
+    forksCount,
+    issuesCount,
+    starsCount,
+  } = reposItemData
+  return (
+    <li className="item-card">
+      <img className="item-img" src={avatarUrl} alt={name} />
+      <h1 className="name">{name}</h1>
+      <div className="icon-name i1">
+        <img
+          className="icon"
+          src="https://assets.ccbp.in/frontend/react-js/stars-count-img.png"
+          alt="stars"
+        />
+        <p className="para">{starsCount} stars</p>
+      </div>
+      <div className="icon-name">
+        <img
+          className="icon"
+          src="https://assets.ccbp.in/frontend/react-js/forks-count-img.png"
+          alt="forks"
+        />
+        <p className="para">{forksCount} forks</p>
+      </div>
+      <div className="icon-name">
+        <img
+          className="icon"
+          src="https://assets.ccbp.in/frontend/react-js/issues-count-img.png"
+          alt="open issues"
+        />
+        <p className="para">{issuesCount} open issues</p>
+      </div>
+    </li>
+  )
+}
+
+export default RepositoryItem
