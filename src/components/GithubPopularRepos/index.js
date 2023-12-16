f4deb358f513a5395c494fe7638fbcd7bfce8dbb
Krishna Praveen <sureshradha084@gmail.com>
2023-12-16 06:14:50 +0000

    Builts the heading and languages tabs part of the project

--- src/components/GithubPopularRepos/index.js
+++ src/components/GithubPopularRepos/index.js
@@ -1,3 +1,9 @@
+import {Component} from 'react'
+
+import LanguageFilterItem from '../LanguageFilterItem'
+
+import './index.css'
+
 const languageFiltersData = [
   {id: 'ALL', language: 'All'},
   {id: 'JAVASCRIPT', language: 'Javascript'},
@@ -7,3 +13,31 @@ const languageFiltersData = [
 ]
 
 // Write your code here
+class GithubPopularRepos extends Component {
+  state = {isActiveTabId: languageFiltersData[0].id}
+
+  selectTab = id => {
+    this.setState({isActiveTabId: id})
+  }
+
+  render() {
+    const {isActiveTabId} = this.state
+    return (
+      <div className="bg-container">
+        <h1 className="heading">Popular</h1>
+        <ul className="filter-items-container">
+          {languageFiltersData.map(each => (
+            <LanguageFilterItem
+              isActiveTabId={isActiveTabId === each.id}
+              selectTab={this.selectTab}
+              languageFiltersData={each}
+              key={each.id}
+            />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default GithubPopularRepos
--- src/components/LanguageFilterItem/index.js
+++ src/components/LanguageFilterItem/index.js
@@ -1,1 +1,23 @@
 // Write your code here
+import './index.css'
+
+const LanguageFilterItem = props => {
+  const {languageFiltersData, selectTab, isActiveTabId} = props
+  const {id, language} = languageFiltersData
+  const onSelectItem = () => {
+    selectTab(id)
+  }
+  const clickClass = isActiveTabId ? 'clicked-item' : null
+  return (
+    <button
+      onClick={onSelectItem}
+      type="button"
+      className={`item-btn ${clickClass}`}
+    >
+      <li className="list-item">
+        <p className="item-name">{language}</p>
+      </li>
+    </button>
+  )
+}
+export default LanguageFilterItem
